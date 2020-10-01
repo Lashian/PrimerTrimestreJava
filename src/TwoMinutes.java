22b84850102c9493e9402037627a73d8e2b833f2
Supervisor <Supervisor@edu.com>
2020-10-01 12:42:25 +0200

    Actualizacion, se añade horas

--- src/TwoMinutes.java
+++ src/TwoMinutes.java
@@ -3,22 +3,22 @@ import java.util.Scanner;
 public class TwoMinutes {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		
 		int minutes;
 		int resto;
-		
+		int hours;
+		int rSeconds;
 		Scanner input = new Scanner (System.in);
 		
 		System.out.println("Introduce una cantidad de segundos para calcular: ");
 		
 		int seconds = input.nextInt();
 		
-		minutes = seconds / 60;
-		resto = minutes % 60;
+		hours = seconds / 3600;
+		rSeconds = seconds % 3600;
+		minutes = rSeconds /60;
+		resto = rSeconds % 60;	
 		
-		System.out.println("La cantidad de minutos es: " + minutes + "con un " + resto + "segundos.");
+		System.out.println("La cantidad de horas es: " + hours + "La cantidad de minutos es: " + minutes + "con un " + resto + "segundos.");
 		
 		
 	}
