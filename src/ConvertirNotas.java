1a678cb3d98b30bb2d12ff47b508c3e0514df8f8
Supervisor <Supervisor@edu.com>
2020-10-08 12:49:43 +0200

    .

---
+++ src/ConvertirNotas.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ConvertirNotas {
+     public static void main(String[] args) {
+
+    //Crear un programa que convierta notas numeras a notas alfabeticas
+    // Ej: Un 6 seria un bien
+    // Un 9 seria un sobresaliente..
+         System.out.println("Conversor de notas.");
+         System.out.println("Por favor, introduzca una nota a compartir: ");
+
+         @SuppressWarnings("resource")
+		Scanner input = new Scanner (System.in);
+         double nota = input.nextDouble();
+
+         if (nota < 5) {
+             System.out.println("Insuficiente.");
+
+         }
+         else if (nota <6) {
+             System.out.println("Aprovado.");
+         }
+         else if(nota <7){
+             System.out.println("Suficiente.");
+         }
+         else if(nota < 9) {
+             System.out.println("Notable.");
+
+         }
+         else if(nota <= 10) {
+             System.out.println("Sobresaliente.");
+         }
+     }
+}
