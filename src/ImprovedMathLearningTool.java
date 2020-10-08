d9b68c89bcf5d79b072a45a56d5b8381335ee5da
Supervisor <Supervisor@edu.com>
2020-10-08 12:59:29 +0200

    Cleaned

--- src/ImprovedMathLearningTool.java
+++ src/ImprovedMathLearningTool.java
@@ -10,16 +10,6 @@ public class ImprovedMathLearningTool {
         System.out.println("Bienvenido a Problemas matematicos.");
         System.out.println("Introduce la solucion a la siguiente question: ");
 
- /*       String numerosString = numberA >= numberB ? numberA  + "-" +  numberB : numberB   +"-" +  numberA;
-        int correctAnswer = numberA >= numberB ? numberA - numberB : numberB - numberA;
-        System.out.println(numerosString);
-        int enterAnswer = input.nextInt();
-        System.out.println("La solucion es " + (enterAnswer == correctAnswer ? "correcta" : "incorrecta"));
-   */
-
-    //    System.out.println((numberA > +"-" +  numberB));
-
-
         if (numberA >= numberB) {
 
             System.out.println((numberA  + "-" +  numberB));
@@ -37,7 +27,5 @@ public class ImprovedMathLearningTool {
                 System.out.println("Tu respuesta es correcta!");
                 }
             else System.out.println("Tu respuesta es incorrecta");
-
     } 
 }
-//test
