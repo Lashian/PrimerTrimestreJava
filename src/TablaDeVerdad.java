4862c4212e69d5088473ac365872ea9379228c53
Supervisor <Supervisor@edu.com>
2020-10-08 14:09:44 +0200

    tabla verdad

---
+++ src/TablaDeVerdad.java
@@ -0,0 +1,24 @@
+//Hacer una tabla de verdad
+//int a  t t f f (cuando a es true la b es true, a true b false, a false b true, a false b false
+//int b t f t f
+// puedes ser las dos verdad, una de klasdos verdad, o ninguna verdad
+
+//a && b  t f f f
+
+//! (a && b) f t t t
+
+//!a  f f t t
+
+//!b  f t f t
+
+//!a || !b f t t t
+
+//Mirar diapositivas
+public class TablaDeVerdad {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
