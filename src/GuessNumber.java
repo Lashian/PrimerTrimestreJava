80cad30b7152cbd1055912da6f9cc7c219f9159b
Lashian <Lashian@Lashian-PC>
2020-10-14 15:45:19 +0200

    Juego Guess Number

--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
 
 public class GuessNumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner (System.in);
+		int CorrectNumber = (int) (Math.random()* 100);
+		System.out.println("TEST" + CorrectNumber);
+		System.out.println("Introduce un numero: ");
+		int EnterNumber = 0;
+		EnterNumber = input.nextInt();
+		
+		while (EnterNumber < CorrectNumber) {
+			System.out.println("Muy bajo");
+			EnterNumber = input.nextInt();
+			while(EnterNumber > CorrectNumber) {
+				System.out.println("Muy alto");
+				EnterNumber = input.nextInt();
+			}
+			
+			
+		}
+		System.out.println("Has acertado");
 	}
 
 }
