b8101af340ab26ac09cdce3d069384db08fd5b19
Supervisor <Supervisor@edu.com>
2020-10-16 13:25:05 +0200

    Suffering simulator

--- src/Lineage2Craft.java
+++ src/Lineage2Craft.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Lineage2Craft {
 
@@ -5,12 +6,39 @@ public class Lineage2Craft {
 		// TODO Auto-generated method stub
 
 		int Craft = (int) (Math.random()*100);
+		Scanner input = new Scanner (System.in);
+		System.out.println("Menu");
+		System.out.println("1.- Craftear Arma Dyansty");
+		System.out.println("2.- Craftear Armadura Dynasty");
+		System.out.println("3.- Craftear joyeria Dynasty");
 		
-		if(Craft <5) {
-			System.out.println("You got Fundation Dynasty Sword");
+		System.out.println("Selecciona el tipo de craft que queires realizar: ");
+		int EnterOption = input.nextInt();
+		
+		switch(EnterOption) {
+		
+		case 1: if(Craft <5) {
+			System.out.println("You got Fundation Dynasty weapon");
 		} else if(Craft <60) {
 			System.out.println("Your craft has been successful");
-		}else System.out.println("You failed your craft");
+		}else System.out.println("You failed your craft. " + "A farmear los putos cokes otra vez");
+		break;
+		
+		case 2:if(Craft <5) {
+			System.out.println("You got Fundation Dynasty Armor");
+		} else if(Craft <60) {
+			System.out.println("Your craft has been successful");
+		}else System.out.println("You failed your craft. " + "Venga, a farmear otra vez los Mythril Alloy campeon");
+		break;
+		
+		case 3:if(Craft <5) {
+			System.out.println("You got Fundation Dynasty Jewel");
+		} else if(Craft <70) {
+			System.out.println("Your craft has been successful");
+		}else System.out.println("You failed your craft. " + "Loguea al spoiler que te veo farmeando los Tons otra vez");
+		break;
+		}
+		System.out.println("Gracias por usar a BuenHombre");
 	}
 
 }
