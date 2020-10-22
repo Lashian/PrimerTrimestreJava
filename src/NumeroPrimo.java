501ff474274d51d2135a07e12d49b2a9a4423db5
Lashian <Lashian@Lashian-PC>
2020-10-23 00:18:41 +0200

    Nunca más numeros primos

--- src/NumeroPrimo.java
+++ src/NumeroPrimo.java
@@ -8,23 +8,17 @@ public class NumeroPrimo {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Introce un numero");
 		int enterPrimo = input.nextInt();
-		int uno = 1;
 		int i = 0;
 		
-	/*	for (i = 2; i <= enterPrimo; i++ ) {
+		for (i = 1; i <= enterPrimo; i++ ) {
 			if(((enterPrimo % i) == 0) && ((enterPrimo / i) == 1)) {
-				System.out.println("El numero es primo.");
-				
-			}
-			else if() {
-				
-			}
-
+				System.out.println(i);
+				System.out.println("El numero  es primo.");	
+			} 
 
 			else System.out.println("Numero no es primo.");
-				
-				
-			}*/
+					
+			}
 		}
 
 	}
--- src/NumerosPorTeclado.java
+++ src/NumerosPorTeclado.java
@@ -1,11 +1,23 @@
-//while EnterNumero != 0
+import java.util.Scanner;
+
+//while EnterNumber != 0
 
 // crear programa que sume numeros hasta que entre un 0
 public class NumerosPorTeclado {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner input = new Scanner(System.in);
+		System.out.println("Introce un numero");
+		int enterNumber = input.nextInt();
+		int result = 0;
+		enterNumber = 1;
+		while (enterNumber != 0) {
+			enterNumber = input.nextInt();
+			result += enterNumber;
+			System.out.println("resultado " + (result));
+		}
+		
 	}
 
 }
