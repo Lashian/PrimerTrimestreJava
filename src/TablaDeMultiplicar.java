8dde8a15e9eae41502ec8f225c756d8e3a4bc1ff
Lashian <Lashian@Lashian-PC>
2020-10-24 00:05:22 +0200

    Tabla de multiplicar lista para metodo

---
+++ src/TablaDeMultiplicar.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TablaDeMultiplicar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner (System.in);
+		System.out.println("Tabla de multiplicar. Introduce un numero: ");
+		int EnterNumber = input.nextInt();
+		System.out.println("La tabla del " + EnterNumber + " es: ");
+		int Result = 0;
+		for (int i = 1; i <= 10; i++) {
+			Result = EnterNumber * i;
+			System.out.println(Result);
+		}
+	}
+
+}
