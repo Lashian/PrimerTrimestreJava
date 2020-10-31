073782dbccdf00b5d38c5074896667183778b4f3
Lashian <Lashian@Lashian-PC>
2020-10-31 22:19:30 +0100

    Powered by Abraham

--- src/DisplayPrimeNumbers.java
+++ src/DisplayPrimeNumbers.java
@@ -4,26 +4,22 @@ public class DisplayPrimeNumbers {
 
 		boolean isPrimo = true;
 		int cuenta = 0;
-		//Este bucle incrementa la variable "number" hasta que la variable "cuenta" es 50 
+	
 		for (int number = 1; cuenta <= 50; number++) {
-			//Este bucle comprueba si la variable "number" NO es un numero primo.
-			//Este es mi ejercicio que hicimos anteriormente de comprobar primos
+			isPrimo = true;
 			for (int i = 2; i < number; i++) {
 				if ((number % i) == 0) {
 					isPrimo = false;
-					number++;
-					//En el caso de que sea false, se incrementa  "number" y vuelve a comprobar hasta que da un primo y se sale
+				
 				}
 			}
-			//Si ha llegado aqui significa que la variable "number" es primo y lo imprime
-			System.out.println(number);
-			cuenta++;
-			//Aumentamos "cuenta" ya que es primo 
-
+	
+			if (isPrimo) {
+				System.out.println(number);
+				cuenta++;
+			}
 			
-			//Basicamente lo que he hecho es añadirle un bucle al programa anterior del primo que hicimos
-			//Para que en lugar de introducir un numero por Scanner vaya...
-			//...un bucle vaya añadiendo numeros automaticamente
+
 		}
 	}
 } 
